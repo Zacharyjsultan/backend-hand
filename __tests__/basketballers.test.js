3eb0d3729b0aa42717fc38e3f3281d660c992216
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-12-07 20:54:17 -0800

    left body out of test

--- __tests__/basketballers.test.js
+++ __tests__/basketballers.test.js
@@ -25,7 +25,7 @@ describe('bball routes', () => {
 
   it('GET all ballers', async () => {
     const res = await request(app).get('/basketballers');
-    expect(res.length).toEqual(5);
+    expect(res.body.length).toEqual(5);
     expect(res.body[0]).toEqual({
       id: expect.any(String),
       name: expect.any(String),
