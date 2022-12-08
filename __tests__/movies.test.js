da5cde9cca78cf8321033733526567eee2d57130
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-12-08 11:57:30 -0800

    test for post moobie

---
+++ __tests__/movies.test.js
@@ -0,0 +1,28 @@
+const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+
+describe('rout 4 movies', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('POST movie', async () => {
+    const movie = new Movies({
+      title: 'Earth',
+      year: 2510,
+      rating: 2,
+    });
+    const res = await request(app).post('/movies').send(movie);
+    expect(res.body.title).toEqual(movie.title);
+    expect(res.body.episodes).toEqual(movie.episodes);
+    expect(res.body.rating).toEqual(movie.rating);
+    const count = await Movies.count();
+    expect(count).toEqual(7);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
