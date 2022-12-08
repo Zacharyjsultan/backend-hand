3788ed803c4c0f20e522d61342b8d12ed1b5655e
Zachary Julius Sultan <ZACHARYJSULTAN@GMAIL.COM>
2022-12-07 20:42:20 -0800

    3:1

--- __tests__/basketballers.test.js
+++ __tests__/basketballers.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const { Basketballers } = require('../lib/models/Basketballers');
 
 describe('bball routes', () => {
   beforeEach(() => {
@@ -9,15 +10,17 @@ describe('bball routes', () => {
   });
 
   it('POST bballerssss route', async () => {
-    const baller = {
+    const baller = new Basketballers({
       name: 'Earl The Pearl Monroe',
       rating: 6,
       strength: 'grandmother shot technique',
-    };
+    });
     const res = await request(app).post('/basketballers').send(baller);
     expect(res.body.name).toEqual(baller.name);
     expect(res.body.rating).toEqual(baller.rating);
     expect(res.body.strength).toEqual(baller.strength);
+    const count = await Basketballers.count();
+    expect(count).toEqual(6);
   });
 
   afterAll(() => {
